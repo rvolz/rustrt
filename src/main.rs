bf098381e6d80afb677056d13b58c83587085cbc
Rainer Volz <dev@textmulch.de>
2019-01-03 13:15:39 +0100

    chapter 1 done

--- src/main.rs
+++ src/main.rs
@@ -20,16 +20,20 @@ pub fn tick(env: &Environment, proj: &Projectile) -> Projectile {
 
 
 fn main() {
-  println!("Hello, world!");
+  println!("Chapter 1 exercise");
+  let v = tuple::vector(1.0, 1.0, 0.0);
   let mut p = Projectile {
     position: tuple::point(0.0, 1.0, 0.0),
-    velocity: tuple::vector(1.0, 1.0, 0.0).normalize()
+    velocity: v.normalize()
   };
   let e = Environment {
     gravity: tuple::vector(0.0, -0.1, 0.0),
     wind: tuple::vector(-0.01, 0.0, 0.0)
   };
-  while p.position.get_y() != &0.0f64 {
+  let mut ticks = 0;
+  while p.position.get_y() >= &0.0f64 {
     p = tick(&e, &p);
+    ticks += 1;
+    println!("Ticks {:?}, Position {:?}",ticks, p.position)
   }
 }
